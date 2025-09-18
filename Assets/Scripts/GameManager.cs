b7af774c5917609f99c13e57b6acc3d6fc847d63
HIRO-LENOVO\nomis <nomisan2010@gmail.com>
2025-09-18 16:27:47 +0900

    プレイヤー、スポットライト、カメラの処理を作成した

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,12 +12,24 @@ public enum GameState
 
 public class GameManager : MonoBehaviour
 {
-    static public bool hasSpotLight;   // スポットライトをもっているかどうか
+    public static GameState gameState;      // ゲームのステータス
+    public static bool[] doorsOpenedState;  // ドアの開放状況
+    public static int key1;                 // 鍵１の餅数
+    public static int key2;                 // 鍵２の餅数
+    public static int key3;                 // 鍵３の餅数
+    public static bool[] keyssPickedState;  // 鍵の取得状況
+
+    public static int bill = 10;                 // お札の持ち数
+    public static bool[] itemsPickedState;  // アイテムの取得状況
+                                            // 
+    public static bool hasSpotLight;        // スポットライトをもっているかどうか
+    public static int playerHP = 3;             // プレーヤーのHP
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // まずはゲームは開始状態にする
+        gameState = GameState.playing;
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/SpotLight.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class SpotLight : MonoBehaviour
+{
+    public PlayerController playerCnt;      // PlayerContollerコンポーネント
+    public float rotationSpeed = 20.0f;     // スポットライトの回転速度
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // 寸前までのスポットライトの回転値（Z軸のみ取得）
+        //float currentAngle = transform.eulerAngles.z;
+
+        // プレイヤーの角度
+        float targetAngle = playerCnt.angleZ;
+
+        // ターゲットとなる角度を調整
+        // -90しているのはプレイヤーの絵の向きが基準が下向き（Unityの基準から-90ずれている）なのでそれに合わせるため
+        Quaternion targetRotation = Quaternion.Euler(0, 0, (targetAngle - 90));
+
+        // 現在の回転を（寸前の回転）→（ターゲットの回転）になるように
+        // なめらかに補完する Quaternion.Slerpメソッド
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+}
